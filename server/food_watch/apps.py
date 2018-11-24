d1c35fd25f467a9079de381ca54b99cfe3cd4815
Louis Trezzini <louis.trezzini@ponts.org>
2018-11-24 15:01:02 +0100

    WIP server

---
+++ server/food_watch/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FoodWatchConfig(AppConfig):
+    name = 'food_watch'
--- server/server/urls.py
+++ server/server/urls.py
@@ -1,4 +1,4 @@
-"""baguette URL Configuration
+"""server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from food_watch.router import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('food-watch/', include(food_watch.router.urls)),
 ]
