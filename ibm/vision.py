99e22cc18ddf5010cfc77a81286bead2d2d1ec06
ArnoutDevos <arnoutdev@gmail.com>
2018-11-24 17:29:20 +0100

    Updated to use normal classifier

--- ibm/vision.py
+++ ibm/vision.py
@@ -5,10 +5,13 @@ visual_recognition = VisualRecognitionV3(
     '2018-03-19',
     iam_apikey='znv5zrsn6pxQsFGAOKtqMw6GJrIYz7XSblKEuN-Kih2d')
 
-with open('./image.jpg', 'rb') as images_file:
+with open('./images/images.zip', 'rb') as images_file:
     classes = visual_recognition.classify(
         images_file,
-        classifier_ids=["food"]).get_result()
+        #classifier_ids=["food"]).get_result()
+        classifier_ids=["default"]).get_result()
     #print(json.dumps(classes, indent=2))
-    with open('./response.json', 'w') as outfile:
-        json.dump(classes, outfile)
+    #with open('./response.json', 'w') as outfile:
+    #    json.dump(classes, outfile)
+    with open("./response.json", "w") as outfile:
+        json.dump(classes, outfile, indent=4, sort_keys=True)
