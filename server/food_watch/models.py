b983edb2ba11cf4bc80c38b585bc6de2ebfbcc00
Louis Trezzini <louis.trezzini@ponts.org>
2018-11-25 08:57:26 +0100

    server patches

--- server/food_watch/models.py
+++ server/food_watch/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 from jsonfield import JSONField
-from uuid import uuid4
 
 
-def picture_path(picture_event, filename):
-    return 'picture-events/{0}.jpg'.format(str(uuid4()))
+def picture_path(picture_patch, filename):
+    return 'picture-events/{0}/{1}_{2}.jpg'.format(picture_patch.event.id, picture_patch.x, picture_patch.y)
 
 
 class PictureEvent(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
+class PicturePatch(models.Model):
+    x = models.IntegerField(blank=False, null=False)
+    y = models.IntegerField(blank=False, null=False)
+
     picture = models.ImageField(upload_to=picture_path, blank=False, null=False)
     metadata = JSONField(default=dict)
+
+    event = models.ForeignKey(PictureEvent, on_delete=models.CASCADE, related_name='patches')
